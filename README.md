346344d6513d5157c7fe6be14f5e632e05cb52f4
Sung Won Cho <mikeswcho@gmail.com>
2015-10-04 20:11:24 +1100

    Add README, License, and test

---
+++ README.md
@@ -0,0 +1,3 @@
+# Meteor LOC
+
+Count the lines of code for your Meteor app.
---
+++ test/mloc_test.js
@@ -0,0 +1,5 @@
+describe("sample test", function(){
+  it("is true", function(){
+    expect(true).to.equal(true);
+  });
+});
